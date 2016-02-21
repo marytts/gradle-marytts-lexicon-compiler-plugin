a400d4dea57a97d6a8c9f41d4d63789d743d3f4b
Ingmar Steiner <steiner@coli.uni-saarland.de>
2016-02-21 20:38:05 +0100

    add missing lexicon/lts properties to config

--- src/test/groovy/XydonianConfigTest.groovy
+++ src/test/groovy/XydonianConfigTest.groovy
@@ -1,7 +1,7 @@
 package marytts.language.xy
 
 import marytts.LocalMaryInterface
-import marytts.exceptions.MaryConfigurationException
+import marytts.exceptions.SynthesisException
 
 import static marytts.datatypes.MaryDataType.*
 
@@ -24,7 +24,7 @@ class XydonianConfigTest {
         assert mary.locale == XYDONIAN
     }
 
-    @Test(expectedExceptions = MaryConfigurationException)
+    @Test(expectedExceptions = SynthesisException)
     void canProcessTextToPhonemes() {
         def mary = new LocalMaryInterface()
         mary.locale = XYDONIAN
