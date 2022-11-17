4342e2d018fd5f831d59bd9551b6ed08adeddb76
Ingmar Steiner <steiner@coli.uni-saarland.de>
2022-11-17 10:35:04 +0100

    Init plugin functional test

---
+++ src/test/groovy/de/dfki/mary/lexicon/LexiconPluginFunctionalTest.groovy
@@ -0,0 +1,48 @@
+package de.dfki.mary.lexicon
+
+import org.gradle.testkit.runner.GradleRunner
+import org.testng.annotations.BeforeClass
+import org.testng.annotations.DataProvider
+import org.testng.annotations.Test
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import static org.gradle.testkit.runner.TaskOutcome.UP_TO_DATE
+
+class LexiconPluginFunctionalTest {
+
+    GradleRunner gradle
+
+    @BeforeClass
+    void setUp() {
+        def projectDir = File.createTempDir()
+        new File(projectDir, 'settings.gradle').createNewFile()
+        gradle = GradleRunner.create().withProjectDir(projectDir).withPluginClasspath().forwardOutput()
+        new File(projectDir, 'build.gradle').withWriter {
+            it << this.class.getResourceAsStream('build.gradle')
+        }
+    }
+
+    @DataProvider
+    Object[][] taskNames() {
+        // task name to run, and whether to chase it with a test task named "testName"
+        [
+                ['help', false],
+                ['testPlugin', false],
+                ['compileLexicon', false],
+                ['testLexicon', false]
+        ]
+    }
+
+    @Test(dataProvider = 'taskNames')
+    void testTasks(String taskName, boolean runTestTask) {
+        def gradleArgs = ['--warning-mode', 'all']
+        def result = gradle.withArguments(gradleArgs + [taskName]).build()
+        assert result.task(":$taskName").outcome in [SUCCESS, UP_TO_DATE]
+        if (runTestTask) {
+            def testTaskName = 'test' + taskName.capitalize()
+            result = gradle.withArguments(gradleArgs + [testTaskName]).build()
+            assert result.task(":$taskName").outcome == UP_TO_DATE
+            assert result.task(":$testTaskName").outcome == SUCCESS
+        }
+    }
+}
